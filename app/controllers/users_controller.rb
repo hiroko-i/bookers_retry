94a6ef1486696f619cc0751d15eba8414036e95f
hiroko-i <90242962+hiroko-i@users.noreply.github.com>
2021-11-14 15:01:22 +0000

    add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_user, only: [:edit, :update]
+
   def index
+    @book = Book.new
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
   end
   
   def create
@@ -12,5 +20,24 @@ class UsersController < ApplicationController
   end
   
   def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
   end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+    redirect_to user_path(current_user)
+    end
+  end  
+  
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '5f99fd3e1d0b85d39f305a272f0f332377110cace4b583ea330a1f303821f391ba72abd76627b540dc3f991fb6b39265b820a78fcf2c93a2c79f061d3bfdc047'
